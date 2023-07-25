a271371b9ddf731b6af0bff9e49762534f0db0db
Bunnyan <chatchapol.k@hotmail.com>
2023-07-26 01:34:59 +0700

    add robots, sitemap, page

---
+++ src/app/sitemap.ts
@@ -0,0 +1,14 @@
+import { Metadata, MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap{
+    return [
+        {
+            url: 'http://localhost:3000',
+            lastModified: new Date(),
+        },
+        {
+            url: 'http://localhost:3000/showcase',
+            lastModified: new Date(),
+        },
+    ]
+}
