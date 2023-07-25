58e64221aaa199bc5caafb03559d5ea91a793aa9
Bunnyan <chatchapol.k@hotmail.com>
2023-07-26 02:12:20 +0700

    add config git action

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+//module.exports = nextConfig
+
+const isProd = process.env.NODE_ENV === 'production';
+module.exports = {
+  reactStrictMode: true,
+  assetPrefix: isProd ? '/bunnyanport/' : '',
+};
